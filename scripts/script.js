93eb082d619f857bf52de18c9e7f8ed5031289e1
Tom He <the18@my.bcit.ca>
2023-10-31 14:24:15 -0700

    restructured files to match the structure done in lecture and configured firebase login

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
-function sayHello() {
-    
+function logout() {
+  firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+      console.log("logging out user");
+    }).catch((error) => {
+      // An error happened.
+    });
 }
-//sayHello();
