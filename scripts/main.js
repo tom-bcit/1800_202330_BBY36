07c937997a28af54eebc4c739f378347edf626bc
Danial Hadady <danialhadady@gmail.com>
2023-11-03 14:26:17 -0700

    Search bar Struggles

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,30 @@
+import {
+    addSearchEventListener
+    
+} from "./search.js";
+
 function getNameFromAuth() {
-  firebase.auth().onAuthStateChanged(user => {
-      // Check if a user is signed in:
-      if (user) {
-          // Do something for the currently logged-in user here: 
-          console.log(user.uid); //print the uid in the browser console
-          console.log(user.displayName);  //print the user name in the browser console
-          userName = user.displayName;
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) {
+            // Do something for the currently logged-in user here: 
+            console.log(user.uid); //print the uid in the browser console
+            console.log(user.displayName);  //print the user name in the browser console
+            userName = user.displayName;
 
-          // method #1:  insert with JS
-          document.getElementById("name-goes-here").innerText = userName;    
+            // method #1:  insert with JS
+            document.getElementById("name-goes-here").innerText = userName;
 
-          //method #2:  insert using jquery
-          //$("#name-goes-here").text(userName); //using jquery
+            //method #2:  insert using jquery
+            //$("#name-goes-here").text(userName); //using jquery
 
-          //method #3:  insert using querySelector
-          //document.querySelector("#name-goes-here").innerText = userName
+            //method #3:  insert using querySelector
+            //document.querySelector("#name-goes-here").innerText = userName
 
-      } else {
-          // No user is signed in.
-      }
-  });
+        } else {
+            // No user is signed in.
+        }
+    });
 }
 getNameFromAuth(); //run the function
 
@@ -31,12 +36,12 @@ function displayCardsDynamically(collection) {
     let cardTemplate = document.getElementById("spaceCardTemplate"); // Retrieve the HTML element with the ID "spaceCardTemplate" and store it in the cardTemplate variable. 
 
     db.collection(collection).get()   //the collection called "spaces"
-        .then(allSpaces=> {
+        .then(allSpaces => {
             //var i = 1;  //Optional: if you want to have a unique ID for each space
             allSpaces.forEach(doc => { //iterate thru each doc
                 var title = doc.data().name;       // get value of the "name" key
                 var details = doc.data().details;  // get value of the "details" key
-				var spaceCode = doc.data().code;    //get unique ID to each space to be used for fetching right image
+                var spaceCode = doc.data().code;    //get unique ID to each space to be used for fetching right image
                 var spaceStatus = doc.data().status; //gets the status field
                 var docID = doc.id;
                 let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
@@ -46,7 +51,7 @@ function displayCardsDynamically(collection) {
                 newcard.querySelector('.card-status').src = `./images/${spaceStatus}.jpg`;
                 newcard.querySelector('.card-text').innerHTML = details;
                 newcard.querySelector('.card-image').src = `./images/${spaceCode}.jpg`; //Example: NV01.jpg
-                newcard.querySelector('a').href = "eachSpace.html?docID="+docID;
+                newcard.querySelector('a').href = "eachSpace.html?docID=" + docID;
 
                 //Optional: give unique ids to all elements for future use
                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
@@ -62,3 +67,6 @@ function displayCardsDynamically(collection) {
 }
 
 displayCardsDynamically("spaces");  //input param is the name of the collection
+
+
+addSearchEventListener();
