2bdfa0b74ffe0ddd3e4c11b349e98d954fb5f906
caitlynchen <caitlyn_xin@163.com>
2023-11-02 23:15:52 -0700

    modify search.html  style.css script.js

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,36 @@ function logout_confir(){
     window.location.href = "./logout.html";
   }
 }
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function myFunction() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
+  }
+}
+function myFunction_ul() {
+  const input = document.querySelector(".form-control");
+  const dropdown = document.getElementById("myUL");
+  
+  if (input.value.length > 0) {
+      dropdown.classList.remove("hidden");
+  } else {
+      dropdown.classList.add("hidden");
+  }
+}
