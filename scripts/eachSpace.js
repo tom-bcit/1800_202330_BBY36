1c4b050ae8eb4721a7699292f586cfa2ff328947
caitlynchen <caitlyn_xin@163.com>
2023-11-14 16:32:47 -0800

    modify eachspace html javascript

--- scripts/eachSpace.js
+++ scripts/eachSpace.js
@@ -1,3 +1,4 @@
+
 function displaySpaceInfo() {
   let params = new URL( window.location.href ); //get URL of search bar
   let ID = params.searchParams.get( "docID" ); //get value for key "id"
@@ -40,9 +41,43 @@ function saveSpaceDocumentIDAndRedirect(){
 }
 
 function myMap() {
-  var mapProp= {
-    center:new google.maps.LatLng(spacelatitude,spacelongitude),
-    zoom:18,
-  };
-  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  // Retrieve ID from the URL parameters just like in displaySpaceInfo
+  let params = new URL(window.location.href);
+  let ID = params.searchParams.get("docID");
+
+  if (ID) { // Make sure ID is not null or undefined
+    db.collection("spaces").doc(ID).get().then(doc => {
+      if (doc.exists) {
+        var spaceName = doc.data().name;
+        var mapProp;
+        if (spaceName == "SE12 - Room 1") {
+          mapProp = {
+            center: new google.maps.LatLng(49.25001012909419, -123.00152632908595),
+            zoom: 18,
+          };
+        } else if (spaceName == "SW1 - Room 1") {
+          mapProp = {
+            center: new google.maps.LatLng(49.251014057113906, -123.00266313271943),
+            zoom: 18,
+          };
+        } else if (spaceName == "NE1 - Room 1") {
+          mapProp = {
+            center: new google.maps.LatLng(49.25411087424854, -123.00095403610528),
+            zoom: 18,
+          };
+        }
+
+        // Now that mapProp is defined and we have confirmed the document exists, instantiate the map
+        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+      } else {
+        console.log("No such document!");
+      }
+    }).catch(error => {
+      console.log("Error getting document:", error);
+    });
+  } else {
+    console.log("Document ID is not provided in the URL parameters.");
   }
+}
+window.addEventListener('load', myMap);
+
